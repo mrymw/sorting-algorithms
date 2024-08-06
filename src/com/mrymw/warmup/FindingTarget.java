2655b0dea93d19819fbf201750d7bc15d009a0b9
Maryam <mrymcodes@gmail.com>
2024-08-06 10:21:23 +0300

    added another method

--- src/com/mrymw/warmup/FindingTarget.java
+++ src/com/mrymw/warmup/FindingTarget.java
@@ -3,12 +3,21 @@ package com.mrymw.warmup;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class FindingTarget {
     public static int findTarget(int[] array, int target) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int s : array) {
             list.add(s);
         }
         return list.indexOf(target);
+    }
+    public static  int finding(int[] array, int target) {
+        for (int i = 0; i<array.length; i++) {
+            if(target==array[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/com/mrymw/warmup/Main.java
+++ src/com/mrymw/warmup/Main.java
@@ -1,11 +1,13 @@
 package com.mrymw.warmup;
 
 import static com.mrymw.warmup.FindingTarget.findTarget;
+import static com.mrymw.warmup.FindingTarget.finding;
 
 public class Main {
     public static void main(String[] args) {
         int[] array = {4,6,7,2,3};
-        int target = 2;
+        int target = 7;
         System.out.println(findTarget(array, target));
+        System.out.println(finding(array, target));
     }
 }
